ccff90fa3c37580b4ed470440aa7ec8b4c70a4d7
sindhu rani goli <saisindhurani@gmail.com>
2020-04-08 22:33:13 -0500

    commit

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const mongoose = require('mongoose');
+const dev_db_url = 'mongodb+srv://sindhu29:saisindhurani@cluster0-htg5e.mongodb.net/local_library?retryWrites=true&w=majority'
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true  ,   useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
